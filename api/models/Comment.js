32d8a79e415e4c806000fbbf023aa18bef39920a
Timur Kim <kimtimur01@gmail.com>
2022-09-27 23:04:10 +0600

    LB-8 Created comments model. (Api)

---
+++ api/models/Comment.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    post: {
+        type: Schema.Types.ObjectId,
+        ref: "Post",
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    datetime: Date
+});
+
+const Comment = mongoose.model("Comment", CommentSchema);
+
+module.exports = Comment;
