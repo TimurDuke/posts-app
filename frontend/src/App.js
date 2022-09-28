4cda9090acad6f2e3085baba1dcf14348dc8f70e
Timur Kim <kimtimur01@gmail.com>
2022-09-28 21:08:21 +0600

    LB-12 Added a user registration form. (Front)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Layout from "./components/UI/Layout/Layout";
 import {Route, Switch} from "react-router-dom";
 import PostsPage from "./containers/PostsPage/PostsPage";
+import Register from "./containers/Register/Register";
 
 const App = () => {
     return (
         <Layout>
             <Switch>
                 <Route path="/" exact component={PostsPage} />
+                <Route path="/register" exact component={Register}/>
                 <Route render={() => <h1>Not Found</h1>} />
             </Switch>
         </Layout>
---
+++ frontend/src/containers/Register/Register.js
@@ -0,0 +1,132 @@
+import React, {useEffect, useState} from 'react';
+import {Link as RouterLink} from 'react-router-dom';
+import {makeStyles} from "tss-react/mui";
+import {Avatar, Container, Grid, Typography, Link} from "@mui/material";
+import {LockOutlined} from "@mui/icons-material";
+import {useDispatch, useSelector} from "react-redux";
+
+import FormElement from "../../components/UI/Form/FormElement/FormElement";
+import ButtonWithProgress from "../../components/UI/ButtonWithProgress/ButtonWithProgress";
+// import {clearRegisterErrors, registerUser} from "../../store/actions/usersActions";
+
+const useStyles = makeStyles()(theme => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        background: '#fff',
+        padding: '0 15px'
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: `green !important`,
+    },
+    form: {
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: `${theme.spacing(2, 0)} !important`,
+    }
+}));
+
+const Register = () => {
+    const { classes } = useStyles();
+    const dispatch = useDispatch();
+
+    // const error = useSelector(state => state.users.registerError);
+    // const registerLoading = useSelector(state => state.users.registerLoading);
+
+    const [user, setUser] = useState({
+        username: '',
+        password: '',
+    });
+
+    // useEffect(() => {
+    //     return () => {
+    //         dispatch(clearRegisterErrors());
+    //     }
+    // }, [dispatch]);
+
+    const inputChangeHandler = e => {
+        const {name, value} = e.target;
+
+        setUser(prev => ({...prev, [name]: value}));
+    };
+
+    const submitFormHandler = e => {
+        e.preventDefault();
+
+        // dispatch(registerUser({...user}));
+    };
+
+    // const getFieldError = fieldName => {
+    //     try {
+    //         return error.errors[fieldName].message;
+    //     } catch {
+    //         return undefined;
+    //     }
+    // };
+
+    return (
+        <Container maxWidth="xs">
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlined/>
+                </Avatar>
+                <Typography component="h1" variant="h6">
+                    Sign up
+                </Typography>
+
+                <Grid
+                    component="form"
+                    onSubmit={submitFormHandler}
+                    container
+                    spacing={2}
+                >
+                    <FormElement
+                        name='username'
+                        label='Username'
+                        required={true}
+                        onChange={inputChangeHandler}
+                        value={user.username}
+                        // error={getFieldError('username')}
+                    />
+                    <FormElement
+                        name='password'
+                        label='Password'
+                        type='password'
+                        required={true}
+                        onChange={inputChangeHandler}
+                        value={user.password}
+                        // error={getFieldError('password')}
+                    />
+
+                    <Grid item xs={12}>
+                        <ButtonWithProgress
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="success"
+                            className={classes.submit}
+                            // loading={registerLoading}
+                            // disabled={registerLoading}
+                        >
+                            Sign Up
+                        </ButtonWithProgress>
+                    </Grid>
+                </Grid>
+
+                <Grid container justifyContent="flex-end">
+                    <Grid item>
+                        <Link style={{color: "green"}} component={RouterLink} to="/login">
+                            Already have an account? Sign in
+                        </Link>
+                    </Grid>
+                </Grid>
+            </div>
+        </Container>
+    );
+};
+
+export default Register;
